99d417cce427cffa17780609339b05fabfb3cc87
Jordan Juel <jordan.juel@gmail.com>
2021-08-24 15:53:29 -0500

    Concise code shown in book for led

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -9,57 +9,16 @@ use volatile::Volatile;
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let mut half_period = 50_u16;
-    let v_half_period = Volatile::new(&mut half_period);
+    let ms = 50_u8;
 
     loop {
-        // Start 0
-        leds[0].on().ok();
+        for curr in 0..8 {
+            let next = (curr + 1) % 8;
 
-        delay.delay_ms(v_half_period.read());
-        leds[0].off().ok();
-        leds[1].on().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[2].on().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[1].off().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[3].on().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[2].off().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[4].on().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[3].off().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[5].on().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[4].off().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[6].on().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[5].off().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[7].on().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[6].off().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[0].on().ok();
-
-        delay.delay_ms(v_half_period.read());
-        leds[7].off().ok();
+            leds[next].on().ok();
+            delay.delay_ms(ms);
+            leds[curr].off().ok();
+            delay.delay_ms(ms);
+        }
     }
 }
