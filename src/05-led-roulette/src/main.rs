031cbf0670f92d1275e4e73066b1718297b3e03a
Jordan Juel <jordan.juel@gmail.com>
2021-08-24 15:38:49 -0500

    My code for figuring out the led roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,64 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
+use volatile::Volatile;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut half_period = 50_u16;
+    let v_half_period = Volatile::new(&mut half_period);
+
+    loop {
+        // Start 0
+        leds[0].on().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[0].off().ok();
+        leds[1].on().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[2].on().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[1].off().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[3].on().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[2].off().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[4].on().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[3].off().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[5].on().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[4].off().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[6].on().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[5].off().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[7].on().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[6].off().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[0].on().ok();
+
+        delay.delay_ms(v_half_period.read());
+        leds[7].off().ok();
+    }
 }
